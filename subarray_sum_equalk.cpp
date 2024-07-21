4a069f4b1214c1a277d1533d3df0e82275f57b56
JaiyatiNanda <81139963+JaiyatiNanda@users.noreply.github.com>
2024-07-21 20:47:29 +0530

    Create subarray_sum_equalk.cpp

---
+++ subarray_sum_equalk.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int,int>mpp;
+        mpp[0]=1;
+        int cnt=0; int prefixsum=0;
+        for(int i=0;i<nums.size();i++){
+            prefixsum+=nums[i];
+            int rem=prefixsum-k;
+            cnt+=mpp[rem];
+            mpp[prefixsum]+=1;
+
+        }return cnt;
+
+        
+    }
+};
